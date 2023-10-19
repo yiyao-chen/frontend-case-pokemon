366386416f99364dc9a9c4a1b4348b87ae4f45fb
Yiyao Chen <yiyaoc@ifi.uio.no>
2023-10-19 21:51:58 +0200

    Minor change on design

--- src/components/InfoContainer.tsx
+++ src/components/InfoContainer.tsx
@@ -10,33 +10,16 @@ const InfoContainer: React.FunctionComponent<Props> = ({
   pokemon: poke,
   isFlipped,
 }) => {
-  console.log("isFlipped");
-  console.log(isFlipped);
   if (poke) {
-    console.log(poke);
-
     if (isFlipped) {
       // back side
       return (
         <div className="infoContainer">
-          <h1>{poke.name}</h1>
-
-          <div>
-            <h3>Abilities:</h3>
-            {poke.abilities.map((ability, index) => (
-              <div className="ability" key={index}>
-                <p>{ability.ability.name}</p>
-              </div>
-            ))}
-          </div>
-
-          <div>
-            <h3>Moves:</h3>
-            {poke.moves
-              .filter((_, index) => index < 2) // get first two moves
-              .map((move, index) => (
-                <p>{move.move.name}</p>
-              ))}
+          <div className="imageContainer-back">
+            <img
+              src={poke.sprites.other["official-artwork"].front_default}
+              alt={`${poke.name} illustration`}
+            />
           </div>
         </div>
       );
@@ -44,13 +27,21 @@ const InfoContainer: React.FunctionComponent<Props> = ({
       // front side
       return (
         <div className="infoContainer">
+          <h1>{poke.name}</h1>
+          <div className="types">
+            {poke.types.map((type, index) => (
+              <div className="type" key={index}>
+                <p>{type.type.name}</p>
+              </div>
+            ))}
+          </div>
+
           <div className="imageContainer">
             <img
               src={poke.sprites.other.dream_world.front_default}
               alt={`${poke.name} illustration`}
             />
           </div>
-          <h1>{poke.name}</h1>
 
           <div className="attributes">
             <p>NO. {poke.id}</p>
@@ -59,31 +50,29 @@ const InfoContainer: React.FunctionComponent<Props> = ({
           </div>
 
           <div className="abilities">
-            <h3>Abilities:</h3>
-            {poke.abilities.map((ability, index) => (
-              <div className="ability" key={index}>
-                <p>{ability.ability.name}</p>
-              </div>
-            ))}
+            <div className="ability-header">
+              <h3>Abilities:</h3>
+            </div>
+            <div className="ability-list">
+              {poke.abilities.map((ability, index) => (
+                <div className="ability" key={index}>
+                  <p>{ability.ability.name}</p>
+                </div>
+              ))}
+            </div>
           </div>
 
           <div className="moves">
             <h3>Moves:</h3>
-            {poke.moves
-              .filter((_, index) => index < 2) // get first two moves
-              .map((move, index) => (
-                <div className="move" key={index}>
-                  <p>{move.move.name}</p>
-                </div>
-              ))}
-          </div>
-
-          <div className="types">
-            {poke.types.map((type, index) => (
-              <div className="type" key={index}>
-                <p>{type.type.name}</p>
-              </div>
-            ))}
+            <div className="move-list">
+              {poke.moves
+                .filter((_, index) => index < 2) // get first two moves
+                .map((move, index) => (
+                  <div className="move" key={index}>
+                    <p>{move.move.name}</p>
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       );
